6bb871c0d00536b49e75850af7695e06b262ca5d
chrisrzhou <chrisrzhou@pm.me>
2022-11-28 01:41:57 -0800

    refactor to updated uinix-ui, guard next window undefined error

--- system/create-system.js
+++ system/create-system.js
@@ -3,4 +3,8 @@ import {createSystem as _createSystem} from 'uinix-ui';
 import {config} from './config.js';
 import {system} from './system.js';
 
-export const createSystem = () => _createSystem(system, config);
+export const createSystem = () => {
+  if (typeof window !== 'undefined') {
+    _createSystem(system, config);
+  }
+};
